facd6bca80a22e96a94a760cf699bb1995c591e5
Erica Lee <Erica@ericas-macbook-pro-3.local>
2015-12-31 04:47:49 -0600

    first time

--- Tips/SettingViewController.swift
+++ Tips/SettingViewController.swift
@@ -10,18 +10,103 @@ import UIKit
 
 class SettingViewController: UIViewController {
 
+    @IBOutlet weak var TipSlider1: UISlider!
+    @IBOutlet weak var TipSlider2: UISlider!
+    @IBOutlet weak var TipSlider3: UISlider!
+    @IBOutlet weak var TipPercentageLabel1: UILabel!
+    @IBOutlet weak var TipPercentageLabel2: UILabel!
+    @IBOutlet weak var TipPercentageLabel3: UILabel!
+    @IBOutlet weak var TipControl: UISegmentedControl!
+    var attr = NSDictionary(object: UIFont(name: "HelveticaNeue-Bold", size: 16.0)!, forKey: NSFontAttributeName) //font
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        //load sliders and labels with default values
+        if let percentage1 = defaults.stringForKey("Default_Percentage1"){
+            TipSlider1.value = (NSString(string: percentage1).floatValue)
+            TipPercentageLabel1.text = percentage1 + "%"
+            TipControl.setTitle(percentage1 + "%", forSegmentAtIndex: 0)
+        }
+        
+        if let percentage2 = defaults.stringForKey("Default_Percentage2"){
+            TipSlider2.value = (NSString(string: percentage2).floatValue)
+            TipPercentageLabel2.text = percentage2 + "%"
+            TipControl.setTitle(percentage2 + "%", forSegmentAtIndex: 1)
+
+        }
+        
+        if let percentage3 = defaults.stringForKey("Default_Percentage3"){
+            TipSlider3.value = (NSString(string: percentage3).floatValue)
+            TipPercentageLabel3.text = percentage3 + "%"
+            TipControl.setTitle(percentage3 + "%", forSegmentAtIndex: 2)
+
+        }
+        
+        
+        //setting tip control slider
+        let index = defaults.integerForKey("Default_Index")
+        TipControl.selectedSegmentIndex = index
+        TipControl.setTitleTextAttributes(attr as [NSObject : AnyObject], forState: .Normal)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    //When the slider is edited for 1st option
+    @IBAction func TipSlider1Slided(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        //change the slider value
+        let currVal1 = Int(TipSlider1.value)
+        TipPercentageLabel1.text = "\(currVal1)%"
+        
+        defaults.setObject(currVal1, forKey: "Default_Percentage1")
+        //defaults.synchronize() //not sure if this is needed?
+        
+        TipControl.setTitle(defaults.stringForKey("Default_Percentage1")! + "%", forSegmentAtIndex: 0)
+    }
+    
+    @IBAction func TipSlider2Slided(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        let currVal2 = Int(TipSlider2.value)
+        TipPercentageLabel2.text = "\(currVal2)%"
+        
+        defaults.setObject(currVal2, forKey: "Default_Percentage2")
+        
+        TipControl.setTitle(defaults.stringForKey("Default_Percentage2")! + "%", forSegmentAtIndex: 1)
+    }
+    
+    @IBAction func TipSlider3Slided(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        
+        let currVal3 = Int(TipSlider3.value)
+        TipPercentageLabel3.text = "\(currVal3)%"
+        
+        defaults.setObject(currVal3, forKey: "Default_Percentage3")
+        
+        TipControl.setTitle(defaults.stringForKey("Default_Percentage3")! + "%", forSegmentAtIndex: 2)
+    }
+    
+    
+    //when user selects default tip percentage control
+    @IBAction func TipControlTap(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let Default_idx = TipControl.selectedSegmentIndex
+        defaults.setObject(Default_idx, forKey: "Default_Index")
+        
+    }
+    
+    @IBAction func onTap2(sender: AnyObject) {
+        view.endEditing(true)
+    }
     /*
     // MARK: - Navigation
 
--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -5,21 +5,198 @@
 //  Created by Erica Lee on 12/27/15.
 //  Copyright © 2015 EricaLee. All rights reserved.
 //
+// BUGS TO FIX : Colors!!! The first $ does not match the typed view text box color
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var BillField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var InitialView: UIView!
+    @IBOutlet weak var TypedView: UIView!
+    var moved = false //for animation
+    @IBOutlet weak var SplitPicker: UIPickerView!
+    var pickerData = ["1","2","3","4","5","6","7","8","9","10"] //for splitting people
+    var attr = NSDictionary(object: UIFont(name: "HelveticaNeue-Bold", size: 16.0)!, forKey: NSFontAttributeName) //font
+    var split = 1.0
+    var tipPercentages = [0.18, 0.2, 0.22] //default tip values
+    
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        split = Double(row) + 1.0
+
+        InitialView.bringSubviewToFront(InitialView)
+        //update Total when picker is selected
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: BillField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = (billAmount + tip)/split
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+   // func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+       // return pickerData[row]
+   // }
+    
+    func pickerView(pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let titleData = pickerData[row]
+        let myTitle = NSAttributedString(string: titleData, attributes: [NSFontAttributeName:UIFont(name: "HelveticaNeue-Bold", size: 20.0)!,NSForegroundColorAttributeName:UIColor(red:0.10, green:0.00, blue:0.07, alpha:1.0)])
+        return myTitle
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Tip Calculator"
+        
+        //Setting up the initial bill amount
+        BillField.attributedPlaceholder = NSAttributedString(string:"$",
+            attributes:[NSForegroundColorAttributeName: UIColor(red:0.10, green:0.00, blue:0.07, alpha:0.5)])
+        
+        //setting Split Picker
+        self.SplitPicker.dataSource = self;
+        self.SplitPicker.delegate = self;
+
+        // Initialize tip labels and total labels
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        self.InitialView.alpha = 1
+        self.TypedView.alpha = 0
+        
+        // Make keyboard pop up when user opens the app
+        BillField.becomeFirstResponder()
+        
+        //setting TipControl
+        tipControl.setTitleTextAttributes(attr as [NSObject : AnyObject], forState: .Normal)
+
+        //get selected value from the split picker
+        SplitPicker.selectRow(0, inComponent: 0, animated: true)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //event for user typing in tip amount and selecting the tip control and when picker is tapped
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        
+        //once user starts typing and it hasn't been moved, Typed View pops up
+        if !BillField.text!.isEmpty && !moved  {
+            UIView.animateWithDuration(0.4, delay: 0, options: .CurveEaseOut, animations: {
+                var IntViewFrame = self.InitialView.frame
+                IntViewFrame.origin.y -= IntViewFrame.size.height
+                self.InitialView.frame = IntViewFrame
+                }, completion: { finished in
+            })
+            moved = true
+            self.TypedView.alpha = 1
+        }
+        
+        if BillField.text!.isEmpty && moved {
+            UIView.animateWithDuration(0.4, delay: 0, options: .CurveEaseOut, animations: {
+                var IntViewFrame = self.InitialView.frame
+                IntViewFrame.origin.y += IntViewFrame.size.height
+                self.InitialView.frame = IntViewFrame
+                }, completion: { finished in
+            })
+            moved = false
+            self.TypedView.alpha = 0
+        }
+        
+        //UIView.animateWithDuration(0.4, animations: {
+            // This causes first view to fade in and second view to fade out
+          //  self.TypedView.alpha = 1
+        //})
+        
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmount = NSString(string: BillField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = (billAmount + tip)/split
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        //Retrieve the Default Tip Percentages and set it to tipControl
+        
+        var defaults: NSUserDefaults = NSUserDefaults.standardUserDefaults()
 
 
+        if let percentage1 = defaults.stringForKey("Default_Percentage1")
+        {
+            //Update Segmented Control to stored default tip values
+            tipControl.setTitle(percentage1 + "%", forSegmentAtIndex: 0)
+            tipPercentages[0] = (NSString(string: percentage1).doubleValue)/100
+
+        }
+        
+        if let percentage2 = defaults.stringForKey("Default_Percentage2")
+        {
+            
+            tipControl.setTitle(percentage2 + "%", forSegmentAtIndex: 1)
+            tipPercentages[1] = (NSString(string: percentage2).doubleValue)/100
+            
+        }
+        
+        if let percentage3 = defaults.stringForKey("Default_Percentage3")
+        {
+            
+            tipControl.setTitle(percentage3 + "%", forSegmentAtIndex: 2)
+            tipPercentages[2] = (NSString(string: percentage3).doubleValue)/100
+
+        }
+        
+        //Update Segmented Control selection
+        let index = defaults.integerForKey("Default_Index")
+        tipControl.selectedSegmentIndex = index
+        
+        //Update Bill Total
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: BillField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = (billAmount + tip)/split
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+
+    }
+        
+    
 }
 
